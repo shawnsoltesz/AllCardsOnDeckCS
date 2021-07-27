cfa6a7a44b864bc38ff964eaec3bd7268da1d946
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-07-27 19:59:53 -0400

    Code progress on left/right index and cards

--- Program.cs
+++ Program.cs
@@ -14,13 +14,35 @@ namespace AllCardsOnDeckCS
 
 
             //for rightIndex from numberOfCards - 1 down to 1 do:
-            //leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. See the section "How do we get a random integer")
+            for (var rightIndex = numberOfCards - 1; rightIndex >= 1; rightIndex--)
+            {
 
-            //Now swap the values at rightIndex and leftIndex by doing this:
-            //leftCard = the value from deck[leftIndex]
-            //rightCard = the value from deck[rightIndex]
-            //deck[rightIndex] = leftCard
-            //deck[leftIndex] = rightCard
+
+                //leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. See the section "How do we get a random integer")
+                var randomNumberGenerator = new Random();
+                var leftIndex = randomNumberGenerator.Next(rightIndex);
+                Console.Write(leftIndex);
+
+                //Now swap the values at rightIndex and leftIndex by doing this:
+                //leftCard = the value from deck[leftIndex]
+                var leftCard = deckOfCards[leftIndex];
+
+
+                //rightCard = the value from deck[rightIndex]
+                var rightCard = deckOfCards[rightIndex];
+                Console.Write(rightIndex);
+
+
+                //deck[rightIndex] = leftCard
+                deckOfCards[rightIndex] = leftCard;
+                Console.Write(leftCard);
+
+
+                //deck[leftIndex] = rightCard
+                deckOfCards[leftIndex] = rightCard;
+                Console.Write(rightCard);
+
+            }
 
             //var suit = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
             //var rank = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
