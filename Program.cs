12a87ac91e2ef502816351f9fd6810f58f493a3f
Shawn Soltesz <shawnjsoltesz@gmail.com>
2021-07-27 19:28:58 -0400

    Updating more homework code - commented out loop code for now

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ namespace AllCardsOnDeckCS
     {
         static void Main(string[] args)
         {
-            //var deckOfCards = new List<string>() { "Ace of Hearts", "2 of Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts", "Ace of Clubs", "2 of Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 of Clubs", "9 of Clubs", "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs", "Ace of Diamonds", "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds", "Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades", "10 of Spades", "Jack of Spades", "Queen of Spades", "King of Spades" };
+            var deckOfCards = new List<string>() { "Ace of Hearts", "2 of Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts", "8 of Hearts", "9 of Hearts", "10 of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts", "Ace of Clubs", "2 of Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", "7 of Clubs", "8 of Clubs", "9 of Clubs", "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs", "Ace of Diamonds", "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds", "Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", "8 of Spades", "9 of Spades", "10 of Spades", "Jack of Spades", "Queen of Spades", "King of Spades" };
 
-            //var numberOfCards = deckOfCards.Count;
-            //Console.Write"numberOfCards";
+            var numberOfCards = deckOfCards.Count;
+            Console.Write(numberOfCards);
 
 
             //for rightIndex from numberOfCards - 1 down to 1 do:
@@ -22,11 +22,15 @@ namespace AllCardsOnDeckCS
             //deck[rightIndex] = leftCard
             //deck[leftIndex] = rightCard
 
-            var suit = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
-            var rank = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            //var suit = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
+            //var rank = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
 
-            var deckOfCards = new List<string>() { $"{suit} of {rank}" };
-            System.Console.WriteLine "deckofcards";
+            //var deckOfCards = new List<string>() { $"{suit} of {rank}" };
+
+            //Console.WriteLine(deckOfCards);
+
+
+
 
 
         }
